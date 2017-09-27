62e882e493d57a3d398e3c2d79f77accb7571b98
Tane Piper <tane@backbase.com>
2017-09-27 21:22:23 +0200

    Improve install of dependencies in blueprints

--- utilities/takeoff/docker-compose-build.js
+++ utilities/takeoff/docker-compose-build.js
@@ -6,14 +6,37 @@ const shellUtils = require('./../lib/shell-utils');
 let sleep = 'sleep 5';
 if (process.platform === 'win32') sleep = 'sleep -s 5';
 
+let cloneRepo = 'https://github.com/takeoff-env/takeoff-blueprint-basic.git';
+let envName = 'takeoff';
+
+if (argv.env) {
+    cloneRepo = argv.env;
+}
+
+if (argv.name) {
+    envName = argv.name;
+}
+
 const commands = [
-    { cmd: `mkdir -p envs/takeoff`, message: 'Creating environment'},
-    { cmd: `git clone https://github.com/takeoff-env/takeoff-blueprint-basic.git envs/takeoff`, message: 'Cloning default environment'},
-    { cmd: `lerna bootstrap`, message: 'Bootstrapping environments'},
-    { cmd: `docker-compose -f envs/takeoff/docker/docker-compose.dev.yml build --no-cache`, message: 'Running Docker Compose Build' },
-    { cmd: `docker-compose -f envs/takeoff/docker/docker-compose.dev.yml up -d db`, message: 'Triggering database creation' },
+    { cmd: `mkdir -p envs/takeoff`, message: 'Creating environment' },
+    { cmd: `git clone ${cloneRepo} envs/${envName}`, message: 'Cloning default environment' },
+    { cmd: `lerna bootstrap`, message: 'Bootstrapping environments', cwd: `envs/${envName}` },
+    {
+        cmd: `docker-compose -f docker/docker-compose.dev.yml build --no-cache`,
+        message: 'Running Docker Compose Build',
+        cwd: `envs/${envName}`
+    },
+    {
+        cmd: `docker-compose -f docker/docker-compose.dev.yml up -d db`,
+        message: 'Triggering database creation',
+        cwd: `envs/${envName}`
+    },
     { cmd: `${sleep}`, message: 'Waiting for database' },
-    { cmd: `docker-compose -f envs/takeoff/docker/docker-compose.dev.yml stop db`, message: 'Shutting down database' }
+    {
+        cmd: `docker-compose -f docker/docker-compose.dev.yml stop db`,
+        message: 'Shutting down database',
+        cwd: `envs/${envName}`
+    }
 ];
 
 shellUtils.series(
