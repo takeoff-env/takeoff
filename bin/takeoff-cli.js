5f4f8d3d20b8b4f04b7b217904f17d2f8bb1ba0e
Tane Piper <piper.tane@gmail.com>
2017-11-21 10:41:23 +0100

    Add update check and do some cleanup to code, and directory exists for commands

--- bin/takeoff-cli.js
+++ bin/takeoff-cli.js
@@ -27,7 +27,7 @@ const shell = require('shelljs');
 
 const { COMMAND_TABLE_HEADERS } = require('./constants');
 
-const { getPluginsForDir, createTable, h, extractArguments } = require('./lib');
+const h = require('./lib');
 
 // instantiate
 
@@ -40,17 +40,17 @@ const init = async () => {
 
     notifier.notify();
 
-    const pluginPaths = await getPluginsForDir();
+    const pluginPaths = await h.getPluginsForDir();
     pluginPaths.forEach(pluginPath => {
         try {
             const plugin = require(pluginPath);
             plugins.push(plugin);
         } catch (e) {
-            shell.echo(`Unable to load plugin ${pluginPath}`);
+            shell.echo(`Unable to load command ${pluginPath}`);
         }
     });
 
-    const { command, args, opts } = extractArguments(argv);
+    const { command, args, opts } = h.extractArguments(argv);
     const tableValues = [];
 
     if (!command || command === 'help') {
@@ -58,7 +58,7 @@ const init = async () => {
             tableValues.push([command, args, (options || []).map(o => o.option).join('\n'), description]);
         });
 
-        const table = createTable(COMMAND_TABLE_HEADERS, tableValues, {
+        const table = h.createTable(COMMAND_TABLE_HEADERS, tableValues, {
             borderStyle: 0,
             compact: true,
             align: 'left',
@@ -70,7 +70,7 @@ const init = async () => {
 
     const plugin = plugins.find(plugin => plugin.command === argv._[0]);
     if (!plugin) {
-        shell.echo(`Error: Plugin not found`);
+        shell.echo(`Error: command ${argv._[0]} not found`);
         shell.exit(1);
     }
     plugin.handler({ command, args, opts, shell, workingDir, h });
--- plugins/takeoff/destroy-env.js
+++ plugins/takeoff/destroy-env.js
@@ -1,34 +1,45 @@
-#!/usr/bin/env node
+/**
+ * Destroys an environment in a non-reversable way
+ * @module takeoff
+ * @param {object} takeoff The object containing the command, arguments, working directory and dependency injected helpers
+ * @param {string} takeoff.command The command being run
+ * @param {string} takeoff.workingDir The working directory of where the command is run
+ * @param {object} takeoff.args The arguments object passed from the command line
+ * @param {object} takeoff.shell The ShellJS instance for doing shell-based commands
+ * @param {object} takeoff.h The Helper object
+ */
+const handler = async ({ shell, args, workingDir }) => {
+  let [environment] = args.length > 0 ? args : ['default'];
+  const envDir = `${workingDir}/envs/${environment}`;
+
+  if (!shell.test('-e', envDir)) {
+    shell.echo(`The environment ${environment} doesn't exist`);
+    shell.exit(0); // Don't exit 1 as this might break CI workflows
+  }
+
+  const dockerDown = shell.exec(
+    `docker-compose -f ${envDir}/docker/docker-compose.yml down --rmi all`,
+  );
+  if (dockerDown.code !== 0) {
+    shell.echo('Error stopping environments');
+    shell.exit(1);
+  }
+
+  const removeFolder = shell.rm('-rf', `${envDir}`);
+  if (removeFolder.code !== 0) {
+    shell.echo('Error deleting environments');
+    shell.exit(1);
+  }
+
+  shell.echo(`Successfully destroyed ${environment}`);
+  shell.exit(0);
+};
 
 module.exports = {
-    command: 'destroy',
-    description: 'Destroys an environment',
-    options: [],
-    args: '<name>',
-    group: 'takeoff',
-    handler: async ({ shell, args, workingDir }) => {
-
-        let [environment] = args.length > 0 ? args : ['default'];
-        const envDir = `${workingDir}/envs/${environment}`;
-
-        if (!shell.test('-e', envDir)) {
-            shell.echo(`The environment ${environment} doesn't exist`);
-            shell.exit(0);  // Don't exit 1 as this might break CI workflows
-        }
-
-        const dockerDown = shell.exec(`docker-compose -f ${envDir}/docker/docker-compose.yml down --rmi all`);
-        if (dockerDown.code !== 0) {
-            shell.echo('Error stopping environments');
-            shell.exit(1);
-        }
-
-        const removeFolder = shell.rm('-rf', `${envDir}`);
-        if (dockerDown.code !== 0) {
-            shell.echo('Error deleting environments');
-            shell.exit(1);
-        }
-        
-        shell.echo(`Successfully destroyed ${environment}`);
-        shell.exit(0);
-    }
+  command: 'destroy',
+  description: 'Destroys an environment',
+  options: [],
+  args: '<name>',
+  group: 'takeoff',
+  handler,
 };
